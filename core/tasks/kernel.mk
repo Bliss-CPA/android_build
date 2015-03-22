8c9d0772f1aa76885b2f8710f688f5a8a5ce49a3
CPA-Poke <brock.shahan@gmail.com>
2015-03-22 10:45:21 -0400

    Revert "Add missing items from CP and also fix dialog in dumpvar."
    This reverts commit 80dd4c4aa78af623b9f07d517ab84517e21860ed.

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -166,16 +166,7 @@ ifneq ($(USE_CCACHE),)
     ccache := $(strip $(wildcard $(ccache)))
 endif
 
-ifneq ($(TARGET_TC_KERNEL),)
-  ifeq ($(HOST_OS),darwin)
-    KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(ANDROID_BUILD_TOP)/prebuilts/gcc/darwin-x86/arm/arm-eabi-$(TARGET_TC_KERNEL)/bin/arm-eabi-"
-  else
-    KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-$(TARGET_TC_KERNEL)/bin/arm-eabi-"
-  endif
-else
-  KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(ccache) $(KERNEL_TOOLCHAIN_PATH)"
-endif
-
+KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(ccache) $(KERNEL_TOOLCHAIN_PATH)"
 ccache =
 
 define mv-modules
